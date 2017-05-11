6a1ecf996d639061c7eec045e8b975cb4d879bf8
Oleksii Kulishov <oku-ext@censhare.com>
2017-05-11 17:10:06 +0300

    Initialize project

---
+++ src/main/java/com/censhare/BranchProtectorApplication.java
@@ -0,0 +1,12 @@
+package com.censhare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchProtectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BranchProtectorApplication.class, args);
+	}
+}
